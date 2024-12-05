:bug: - Fixed rotation string and normalize bug on several files

classification/Adversarial.py
@@ -185,7 +185,7 @@ if __name__ == "__main__":
     # plot feature importance
     indexes = np.arange(len(labels))
     plt.bar([x for x in range(len(importance))], importance)
-    plt.xticks(indexes, labels, rotation = '48')
+    plt.xticks(indexes, labels, rotation=48)
     plt.savefig('lc/adv/bar-ad-good.png')
     plt.clf()
     plt.cla()
@@ -214,7 +214,7 @@ if __name__ == "__main__":
     # plot feature importance
     indexes = np.arange(len(labels))
     plt.bar([x for x in range(len(importance))], importance)
-    plt.xticks(indexes, labels, rotation = '48')
+    plt.xticks(indexes, labels, rotation=48)
     plt.savefig('lc/adv/bar-ad-fake.png')
     plt.clf()
     plt.cla()

classification/FoolingAI.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
                                                ('cat', categorical_transformer, categorical_features)
                                               ])
 
-    param_lr = [{'fit_intercept':[True,False], 'normalize':[True,False]}]
+    param_lr = [{'fit_intercept':[True,False]}]
 
     models_classification = {
         'lc': {'name': 'Linear Classifier',
@@ -99,7 +99,7 @@ if __name__ == "__main__":
 
     indexes = np.arange(len(labels))
     plt.bar([x for x in range(len(importance))], importance)
-    plt.xticks(indexes, labels, rotation = '48')
+    plt.xticks(indexes, labels, rotation=48)
     plt.savefig('lc/fooling/bar-%s.png'%loss)
     plt.clf()
     plt.cla()

regression/Adversarial.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
 
     models_regression = {
         'lr': {'name': 'Linear Regression',
-               'estimator': LinearRegression(fit_intercept=False,copy_X=True,normalize=False),
+               'estimator': LinearRegression(fit_intercept=False,copy_X=True),
               },
         'fake': {'name':'Custom',
                  'estimator':PolyRegressor(adv='lf'),
@@ -189,7 +189,7 @@ if __name__ == "__main__":
     # plot feature importance
     indexes = np.arange(len(labels))
     plt.bar([x for x in range(len(importance))], importance)
-    plt.xticks(indexes, labels, rotation = '48')
+    plt.xticks(indexes, labels, rotation=48)
     plt.savefig('adv/bar-ad-good.png')
     plt.clf()
     plt.cla()
@@ -218,7 +218,7 @@ if __name__ == "__main__":
     # plot feature importance
     indexes = np.arange(len(labels))
     plt.bar([x for x in range(len(importance))], importance, color='red')
-    plt.xticks(indexes, labels, rotation = '48')
+    plt.xticks(indexes, labels, rotation=48)
     plt.savefig('adv/bar-ad-fake.png')
     plt.clf()
     plt.cla()

regression/FoolingAI.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
                                               ])
     model_reg = ['lr']
 
-    param_lr = [{'fit_intercept':[True,False], 'normalize':[True,False]}]
+    param_lr = [{'fit_intercept':[True,False]}]
 
     models_regression = {
         'lr': {'name': 'Linear Regression',
@@ -99,7 +99,7 @@ if __name__ == "__main__":
 
     indexes = np.arange(len(labels))
     plt.bar([x for x in range(len(importance))], importance)
-    plt.xticks(indexes, labels, rotation = '48')
+    plt.xticks(indexes, labels, rotation=48)
     plt.savefig('lr/fooling/bar-%s.png'%loss)
     plt.clf()
     plt.cla()